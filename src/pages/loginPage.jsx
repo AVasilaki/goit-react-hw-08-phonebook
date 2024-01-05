32ce5ed574a360d5940fe67980de8bbcb5e6d410
AVasilaki <poseydon463@gmail.com>
2024-01-05 09:50:19 +0200

    upd

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,8 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { fetchLoginUser } from '../redux/operation';
-import { useDispatch, useSelector } from 'react-redux';
-import { getUser } from '../redux/selectors';
-import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 const Login = () => {
   const [formData, setFormData] = useState({
@@ -11,9 +9,7 @@ const Login = () => {
   });
 
   const dispatch = useDispatch();
-  const { isLoggedIn } = useSelector(getUser);
-  const navigate = useNavigate();
-  // const token = useSelector(selectToken);
+
   const handleChange = e => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
@@ -22,32 +18,31 @@ const Login = () => {
     e.preventDefault();
     dispatch(fetchLoginUser(formData));
   };
-  useEffect(() => {
-    if (isLoggedIn) {
-      navigate('/contacts');
-    }
-  }, [isLoggedIn, navigate]);
+
   return (
-    <form onSubmit={handleSubmit} className='mb-2 flex flex-col items-start gap-4'>
+    <form
+      onSubmit={handleSubmit}
+      className="mb-2 flex flex-col items-start gap-4"
+    >
       <input
-        className='ml-6 rounded-lg bg-blue-500 px-2 text-white'
-        type='email'
-        name='email'
+        className="ml-6 rounded-lg bg-blue-500 px-2 text-white"
+        type="email"
+        name="email"
         value={formData.email}
         onChange={handleChange}
-        placeholder='email'
+        placeholder="email"
       />
       <input
-        className='ml-6 rounded-lg bg-blue-500 px-2 text-white'
-        type='password'
-        name='password'
+        className="ml-6 rounded-lg bg-blue-500 px-2 text-white"
+        type="password"
+        name="password"
         value={formData.password}
         onChange={handleChange}
-        placeholder='password'
+        placeholder="password"
       />
       <button
-        className='ml-auto rounded-lg bg-blue-500 px-4 py-2 text-white hover:bg-green-600 active:bg-rose-700'
-        type='submit'
+        className="ml-auto rounded-lg bg-blue-500 px-4 py-2 text-white hover:bg-green-600 active:bg-rose-700"
+        type="submit"
       >
         Login
       </button>
